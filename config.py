73d7e799228c5fe39fc15e7ad64f09530993cd78
arvind0252 <167284195+arvind0252@users.noreply.github.com>
2024-09-25 16:56:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BABY-MUSIC/BABYXMUSIC",
+    "https://github.com/arvind0252/BABYXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
