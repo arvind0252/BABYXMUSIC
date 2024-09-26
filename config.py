bc24b73403f410075834f39b33a9d1528a6d6faa
arvind0252 <167284195+arvind0252@users.noreply.github.com>
2024-09-26 07:50:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BABY09_WORLD")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+j6FO8pK8IIkxZDU1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+DaLwM3-VTCdiNmM1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/the_circle_king_j")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
